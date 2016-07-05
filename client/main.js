fac98dca6018026929559049d6b42cc7f1438c0b
Subrata Mal <06.subrata@gmail.com>
2016-07-06 00:15:14 +0530

    unnecessary store reference removed

--- client/main.js
+++ client/main.js
@@ -3,7 +3,6 @@
 var React = require('react');
 var Router = require('react-router');
 var routes = require('./routes');
-var VideoStore = require('./stores/videoStore');
 
 Router.run( routes, function(Handler) {
 	React.render(<Handler/>, document.getElementById('adminApp'));
