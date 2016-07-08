e23294629180bca40da14dcc16ab97b56800bcca
Bikash <bikash@datarpm.com>
2016-07-08 14:18:24 +0530

    new file:   client/components/videos/videoPlayer.js 	new file:   client/components/videos/videos.js 	modified:   .gitignore

---
+++ client/components/videos/videoPlayer.js
@@ -0,0 +1,13 @@
+"use Strict";
+
+var React = require('react');
+
+var VideoPlayer = React.createClass({
+  render(){
+    return(
+      <div>Play Video</div>
+    );
+  }
+});
+
+module.exports = VideoPlayer;
---
+++ client/components/videos/videos.js
@@ -0,0 +1,14 @@
+"use strict";
+
+var React = require('react');
+
+var Videos = React.createClass({
+
+  render(){
+    return(
+      <div>Videos</div>
+    );
+  }
+});
+
+module.exports = Videos;
